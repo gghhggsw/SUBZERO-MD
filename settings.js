c36eef01a59598c43ae394593ba1c3b1b58c05d4
gghhggsw <np060.2773@gmail.com>
2025-09-30 00:24:12 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~19993b123733d6169d2fb163accbe0a92002ace0", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO~0951e4fba398eebceea1b4ddb3758171787a8ff4", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ",", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
