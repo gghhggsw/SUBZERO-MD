6df1c3f3e8e1c54655fcec1651557418edf52c17
gghhggsw <np060.2773@gmail.com>
2025-10-10 10:03:40 +0200

    Update settings.js for subzeroxxxccvg

--- settings.js
+++ settings.js
@@ -1,8 +1,6 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~6540ce55804a97eddf81f67288ff9766213db72c", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ",", // Default Prefix You Can Change To .,*!?+-&%#@
-  CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
+  SESSION_ID: "Fffg",
+  OWNER_NUMBER: "263", 
+  PREFIX: ".",
+  CDN: "https://mrfrankk-cdn.hf.space"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
